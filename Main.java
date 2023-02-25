dd0c79c53b2b1c606dc3b43301a1f1712b5c00e6
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-02-25 19:27:35 +0500

    Generic Class

--- Main.java
+++ Main.java
@@ -2,12 +2,9 @@ package Generics;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new List();
-        list.add("1");
-        list.add(1);
-        list.add(new User());
-        String index0 = (String) list.get(0);
-        int index1 = (int) list.get(1);
-        var user = (User) list.get(2);
+        GenericList<Integer> intList = new GenericList<>();
+        intList.add(1);
+        int a = intList.get(0);
+
     }
 }
