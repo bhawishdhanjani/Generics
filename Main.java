393ee82b78ab170231e605916a04d077b93f890c
Bhavesh Kumar <bhawishdhanjanii@gmail.com>
2023-02-27 22:11:38 +0500

    Wild Card

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         User user2 = new User(20);
         User maxUser = Utils.max(user1,user2);
         System.out.println(maxUser);
-        Utils.print("ABC",1);
+        Utils.printUser(new GenericList<User>());
 
     }
 }
--- Utils.java
+++ Utils.java
@@ -8,4 +8,8 @@ public class Utils {
     public static <K,V> void print(K key , V value){
         System.out.println(key+"="+value);
     }
+    public static void printUser(GenericList<? super User> user){
+        user.add(new User(0));
+
+    }
 }
